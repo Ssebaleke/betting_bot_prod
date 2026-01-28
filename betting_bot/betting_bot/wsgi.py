1f7ab54e00bdae061498833c8ea55505f1af63ca
Ssebaleke John <jsvico100@gmail.com>
2026-01-28 21:11:22 +0300

    fix paths for docker deployment

--- betting_bot/betting_bot/wsgi.py
+++ betting_bot/betting_bot/wsgi.py
@@ -1,6 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "betting_bot.betting_bot.settings")
+# We simplified this from betting_bot.betting_bot.settings to just:
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "betting_bot.settings")
 
 application = get_wsgi_application()
