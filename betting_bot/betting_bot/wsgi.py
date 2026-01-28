b50bbd350f16051445191eca380c23081a6c31bd
Ssebaleke John <jsvico100@gmail.com>
2026-01-28 22:24:51 +0300

    all-files

--- betting_bot/betting_bot/wsgi.py
+++ betting_bot/betting_bot/wsgi.py
@@ -1,7 +1,9 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-# We simplified this from betting_bot.betting_bot.settings to just:
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "betting_bot.settings")
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "betting_bot.betting_bot.settings"
+)
 
 application = get_wsgi_application()
