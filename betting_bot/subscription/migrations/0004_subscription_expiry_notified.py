9057674fd2748b6b4b0b514e922e4d79c0c00187
Ssebaleke John <jsvico100@gmail.com>
2026-03-25 15:57:48 +0300

    fix: add 0004 migration for expiry_notified column (was added to already-applied 0003)

--- betting_bot/subscription/migrations/0004_subscription_expiry_notified.py
+++ betting_bot/subscription/migrations/0004_subscription_expiry_notified.py
@@ -4,13 +4,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("subscription", "0002_alter_subscription_end_date"),
+        ("subscription", "0003_subscription_reminder_sent"),
     ]
 
     operations = [
         migrations.AddField(
             model_name="subscription",
-            name="reminder_sent",
+            name="expiry_notified",
             field=models.BooleanField(default=False),
         ),
     ]
